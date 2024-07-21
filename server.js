18522668e8b0ecdfc32745c835fa89b066775be5
Steve <steve.gitprofile@outlook.com>
2024-07-21 09:47:01 +0200

    configured netlify

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // Path to your JSON file
+const router = jsonServer.router("./data/db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use("/api", router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+module.exports = server;
